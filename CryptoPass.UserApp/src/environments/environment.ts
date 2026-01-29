52e3bab7fb816fbb1c31456e91dab91503ab5860
ur_supp0rter_main <stocker.wie@gmail.com>
2026-01-29 10:12:19 +0100

    refactor: update Dockerfile and environment configurations for production readiness

--- CryptoPass.UserApp/src/environments/environment.ts
+++ CryptoPass.UserApp/src/environments/environment.ts
@@ -2,6 +2,6 @@ import { Environment } from './environment.interface';
 
 export const environment: Environment = {
   production: true,
-  ceramicNodeUrl: 'http://localhost:7007',
+  ceramicNodeUrl: 'https://ceramic.ursupportmain.com',
   enablePayments: false,
 };
